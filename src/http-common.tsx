9fa0e4cffe64adbe223942acc15347b5fc44fbe1
Frann0 <deengo113@gmail.com>
2023-01-22 03:14:36 +0100

    t

--- src/http-common.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export default axios.create({
-    baseURL: process.env.REACT_APP_API !== undefined ? process.env.REACT_APP_API : "http://localhost:5121/",
-    headers: {
-        "Authorization": `Bearer ${localStorage.getItem('token')}`,
-        "Content-type": "application/json"
-    }
-});
